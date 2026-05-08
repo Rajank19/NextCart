9ac400cbccd08b675da3925015724ec50b831993
Rajan Kumar <rajansukul20@gmail.com>
2026-05-08 15:10:29 +0530

    Testing main jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,76 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-
-function ErrorFallback({ error }) {
-
-  return (
-
-    <div
-      style={{
-        background: "black",
-        color: "white",
-        minHeight: "100vh",
-        padding: "40px",
-      }}
-    >
-
-      <h1>React Crash Detected ❌</h1>
-
-      <pre>
-        {String(error)}
-      </pre>
-
-    </div>
-
-  );
-}
-
-class ErrorBoundary extends React.Component {
-
-  constructor(props) {
-
-    super(props);
-
-    this.state = {
-      hasError: false,
-      error: null,
-    };
-  }
-
-  static getDerivedStateFromError(error) {
-
-    return {
-      hasError: true,
-      error,
-    };
-  }
-
-  render() {
-
-    if (this.state.hasError) {
-
-      return (
-        <ErrorFallback
-          error={this.state.error}
-        />
-      );
-    }
-
-    return this.props.children;
-  }
-}
 
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
 
-  <React.StrictMode>
-
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-
-  </React.StrictMode>
+  <h1
+    style={{
+      color: "white",
+      background: "black",
+      minHeight: "100vh",
+      padding: "40px",
+    }}
+  >
+    MAIN JSX WORKING 🚀
+  </h1>
 
 );
