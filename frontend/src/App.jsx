77473d614e1173201497fb39acf436b9f540fa45
Rajan Kumar <rajansukul20@gmail.com>
2026-05-09 12:40:39 +0530

    Fixed payment and cart issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -277,7 +277,14 @@ import {
   Route,
 } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
+
 import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import Orders from "./pages/Orders";
+import Login from "./pages/Login";
+import Checkout from "./pages/Checkout";
 
 function App() {
 
@@ -285,11 +292,37 @@ function App() {
 
     <BrowserRouter>
 
+      <Navbar />
+
       <Routes>
 
         <Route
           path="/"
           element={<Home />}
+        />
+
+        <Route
+          path="/products"
+          element={<Products />}
+        />
+
+        <Route
+          path="/cart"
+          element={<Cart />}
+        />
+
+        <Route
+          path="/orders"
+          element={<Orders />}
+        />
+
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+        <Route
+        path="/checkout"
+        element={<Checkout />}
         />
 
       </Routes>
--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -1,9 +1,342 @@
+// import React, { useState } from "react";
+// import axios from "axios";
+
+// function Checkout() {
+
+//   const total = localStorage.getItem("cartTotal");
+
+//   const [name, setName] = useState("");
+//   const [address, setAddress] = useState("");
+//   const [city, setCity] = useState("");
+//   const [pincode, setPincode] = useState("");
+//   const [mobile, setMobile] = useState("");
+
+//   // PAYMENT FUNCTION
+//   const handlePayment = async () => {
+
+//     // VALIDATION
+//     if (
+//       !name ||
+//       !address ||
+//       !city ||
+//       !pincode ||
+//       !mobile
+//     ) {
+//       alert("Please Fill All Fields ❌");
+//       return;
+//     }
+
+//     try {
+
+//       // CREATE ORDER
+//       const res = await axios.post(
+//         "http://127.0.0.1:8000/payments/create-order",
+//         {
+//           amount: Number(total),
+//         }
+//       );
+
+//       console.log("PAYMENT RESPONSE:", res.data);
+
+//       const order = res.data;
+
+//       // RAZORPAY OPTIONS
+//       const options = {
+
+//         key: "rzp_test_SmZCRQOBXa7YGq",
+
+//         amount: order.amount,
+
+//         currency: order.currency,
+
+//         name: "NextCart",
+
+//         description: "UPI Payment",
+
+//         image:
+//           "https://cdn-icons-png.flaticon.com/512/3081/3081559.png",
+
+//         order_id: order.id,
+
+//         // ENABLE METHODS
+//         method: {
+//           upi: true,
+//           card: true,
+//           netbanking: true,
+//           wallet: true,
+//         },
+
+//         // DIRECT UPI FIRST
+//         config: {
+//           display: {
+
+//             blocks: {
+
+//               upi: {
+//                 name: "Pay Using UPI",
+//                 instruments: [
+//                   {
+//                     method: "upi",
+//                   },
+//                 ],
+//               },
+//             },
+
+//             sequence: [
+//               "block.upi",
+//             ],
+
+//             preferences: {
+//               show_default_blocks: true,
+//             },
+//           },
+//         },
+
+//         // PAYMENT SUCCESS
+//         handler: function (response) {
+
+//           console.log(
+//             "PAYMENT SUCCESS:",
+//             response
+//           );
+
+//           // CREATE ORDER OBJECT
+//           const newOrder = {
+
+//             id: Date.now(),
+
+//             customerName: name,
+
+//             address,
+
+//             city,
+
+//             pincode,
+
+//             mobile,
+
+//             total,
+
+//             paymentId:
+//               response.razorpay_payment_id,
+
+//             orderId:
+//               response.razorpay_order_id,
+
+//             signature:
+//               response.razorpay_signature,
+
+//             date: new Date().toLocaleString(),
+//           };
+
+//           // GET OLD ORDERS
+//           const oldOrders =
+//             JSON.parse(
+//   localStorage.getItem("orders") || "[]"
+// );
+
+//           // SAVE ORDER
+//           localStorage.setItem(
+//             "orders",
+//             JSON.stringify([
+//               ...oldOrders,
+//               newOrder,
+//             ])
+//           );
+
+//           // CLEAR CART
+//           localStorage.removeItem("cartTotal");
+
+//           alert("Payment Successful 🎉");
+
+//           // REDIRECT
+//           window.location.href = "/orders";
+//         },
+
+//         // USER PREFILL
+//         prefill: {
+
+//           name: name,
+
+//           contact: mobile,
+//         },
+
+//         // NOTES
+//         notes: {
+//           address: address,
+//           city: city,
+//           pincode: pincode,
+//         },
+
+//         // UI THEME
+//         theme: {
+//           color: "#a855f7",
+//         },
+//       };
+
+//       // OPEN PAYMENT WINDOW
+//       const razor = new window.Razorpay(options);
+
+//       razor.open();
+
+//     } catch (err) {
+
+//       console.log(
+//         "PAYMENT ERROR:",
+//         err.response?.data || err
+//       );
+
+//       alert("Payment Failed ❌");
+
+//     }
+//   };
+
+//   return (
+//     <div
+//       style={{
+//         background: "#050505",
+//         minHeight: "100vh",
+//         color: "white",
+//         padding: "40px",
+//       }}
+//     >
+
+//       <h1
+//         style={{
+//           textAlign: "center",
+//           marginBottom: "40px",
+//           fontSize: "50px",
+//         }}
+//       >
+//         Checkout 💳
+//       </h1>
+
+//       <div
+//         style={{
+//           maxWidth: "500px",
+//           margin: "auto",
+//           background: "#111",
+//           padding: "30px",
+//           borderRadius: "14px",
+//           boxShadow: "0 8px 20px rgba(0,0,0,0.4)",
+//         }}
+//       >
+
+//         {/* NAME */}
+//         <input
+//           type="text"
+//           placeholder="Full Name"
+//           value={name}
+//           onChange={(e) =>
+//             setName(e.target.value)
+//           }
+//           style={input}
+//         />
+
+//         {/* ADDRESS */}
+//         <input
+//           type="text"
+//           placeholder="Address"
+//           value={address}
+//           onChange={(e) =>
+//             setAddress(e.target.value)
+//           }
+//           style={input}
+//         />
+
+//         {/* CITY */}
+//         <input
+//           type="text"
+//           placeholder="City"
+//           value={city}
+//           onChange={(e) =>
+//             setCity(e.target.value)
+//           }
+//           style={input}
+//         />
+
+//         {/* PINCODE */}
+//         <input
+//           type="text"
+//           placeholder="Pincode"
+//           value={pincode}
+//           onChange={(e) =>
+//             setPincode(e.target.value)
+//           }
+//           style={input}
+//         />
+
+//         {/* MOBILE */}
+//         <input
+//           type="text"
+//           placeholder="Mobile Number"
+//           value={mobile}
+//           onChange={(e) =>
+//             setMobile(e.target.value)
+//           }
+//           style={input}
+//         />
+
+//         {/* TOTAL */}
+//         <h2
+//           style={{
+//             marginTop: "30px",
+//             color: "#22c55e",
+//           }}
+//         >
+//           Total: ₹ {total}
+//         </h2>
+
+//         {/* BUTTON */}
+//         <button
+//           style={btn}
+//           onClick={handlePayment}
+//         >
+//           Pay Now 🚀
+//         </button>
+
+//       </div>
+
+//     </div>
+//   );
+// }
+
+// // INPUT STYLE
+// const input = {
+//   width: "100%",
+//   padding: "14px",
+//   marginTop: "15px",
+//   borderRadius: "10px",
+//   border: "none",
+//   outline: "none",
+//   background: "#1f2937",
+//   color: "white",
+//   fontSize: "15px",
+// };
+
+// // BUTTON STYLE
+// const btn = {
+//   width: "100%",
+//   padding: "14px",
+//   marginTop: "30px",
+//   background: "#a855f7",
+//   color: "white",
+//   border: "none",
+//   borderRadius: "10px",
+//   cursor: "pointer",
+//   fontSize: "18px",
+//   fontWeight: "bold",
+// };
+
+// export default Checkout;
+
 import React, { useState } from "react";
 import axios from "axios";
 
 function Checkout() {
 
-  const total = localStorage.getItem("cartTotal");
+  const total =
+    localStorage.getItem("cartTotal") || 0;
 
   const [name, setName] = useState("");
   const [address, setAddress] = useState("");
@@ -28,17 +361,32 @@ function Checkout() {
 
     try {
 
+      // CHECK RAZORPAY
+      if (!window.Razorpay) {
+        alert("Razorpay SDK Failed To Load ❌");
+        return;
+      }
+
       // CREATE ORDER
       const res = await axios.post(
-        "http://127.0.0.1:8000/payments/create-order",
+        "http://localhost:8000/payments/create-order",
         {
-          amount: Number(total),
+          amount: 500,
         }
       );
 
-      console.log("PAYMENT RESPONSE:", res.data);
+      console.log(
+        "PAYMENT RESPONSE:",
+        res.data
+      );
 
       const order = res.data;
+
+      // CHECK ORDER
+      if (!order.id) {
+        alert("Order Creation Failed ❌");
+        return;
+      }
 
       // RAZORPAY OPTIONS
       const options = {
@@ -58,41 +406,6 @@ function Checkout() {
 
         order_id: order.id,
 
-        // ENABLE METHODS
-        method: {
-          upi: true,
-          card: true,
-          netbanking: true,
-          wallet: true,
-        },
-
-        // DIRECT UPI FIRST
-        config: {
-          display: {
-
-            blocks: {
-
-              upi: {
-                name: "Pay Using UPI",
-                instruments: [
-                  {
-                    method: "upi",
-                  },
-                ],
-              },
-            },
-
-            sequence: [
-              "block.upi",
-            ],
-
-            preferences: {
-              show_default_blocks: true,
-            },
-          },
-        },
-
-        // PAYMENT SUCCESS
         handler: function (response) {
 
           console.log(
@@ -130,10 +443,9 @@ function Checkout() {
           };
 
           // GET OLD ORDERS
-          const oldOrders =
-            JSON.parse(
-  localStorage.getItem("orders") || "[]"
-);
+          const oldOrders = JSON.parse(
+            localStorage.getItem("orders") || "[]"
+          );
 
           // SAVE ORDER
           localStorage.setItem(
@@ -145,6 +457,8 @@ function Checkout() {
           );
 
           // CLEAR CART
+          localStorage.removeItem("cart");
+
           localStorage.removeItem("cartTotal");
 
           alert("Payment Successful 🎉");
@@ -153,7 +467,6 @@ function Checkout() {
           window.location.href = "/orders";
         },
 
-        // USER PREFILL
         prefill: {
 
           name: name,
@@ -161,33 +474,47 @@ function Checkout() {
           contact: mobile,
         },
 
-        // NOTES
         notes: {
+
           address: address,
-          city: city,
-          pincode: pincode,
         },
 
-        // UI THEME
         theme: {
+
           color: "#a855f7",
         },
       };
 
-      // OPEN PAYMENT WINDOW
-      const razor = new window.Razorpay(options);
+      // OPEN RAZORPAY
+      const razorpayObject =
+        new window.Razorpay(options);
 
-      razor.open();
+      // PAYMENT FAILED
+      razorpayObject.on(
+        "payment.failed",
+        function (response) {
+
+          console.log(
+            "FAILED RESPONSE:",
+            response
+          );
+
+          alert(
+            response.error.description
+          );
+        }
+      );
+
+      razorpayObject.open();
 
     } catch (err) {
 
       console.log(
-        "PAYMENT ERROR:",
-        err.response?.data || err
+        "FULL PAYMENT ERROR:",
+        err
       );
 
-      alert("Payment Failed ❌");
-
+      alert("Backend/API Error ❌");
     }
   };
 
@@ -222,7 +549,6 @@ function Checkout() {
         }}
       >
 
-        {/* NAME */}
         <input
           type="text"
           placeholder="Full Name"
@@ -233,7 +559,6 @@ function Checkout() {
           style={input}
         />
 
-        {/* ADDRESS */}
         <input
           type="text"
           placeholder="Address"
@@ -244,7 +569,6 @@ function Checkout() {
           style={input}
         />
 
-        {/* CITY */}
         <input
           type="text"
           placeholder="City"
@@ -255,7 +579,6 @@ function Checkout() {
           style={input}
         />
 
-        {/* PINCODE */}
         <input
           type="text"
           placeholder="Pincode"
@@ -266,7 +589,6 @@ function Checkout() {
           style={input}
         />
 
-        {/* MOBILE */}
         <input
           type="text"
           placeholder="Mobile Number"
@@ -277,7 +599,6 @@ function Checkout() {
           style={input}
         />
 
-        {/* TOTAL */}
         <h2
           style={{
             marginTop: "30px",
@@ -287,7 +608,6 @@ function Checkout() {
           Total: ₹ {total}
         </h2>
 
-        {/* BUTTON */}
         <button
           style={btn}
           onClick={handlePayment}
